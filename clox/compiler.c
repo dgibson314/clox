f8b5ac74c9b406c70a5b47aeab5d95921354c88b
David Gibson <dgibson314@gmail.com>
2023-02-19 20:52:10 -0500

    Merge ch-19-strings

--- clox/compiler.c
+++ clox/compiler.c
@@ -3,6 +3,7 @@
 
 #include "common.h"
 #include "compiler.h"
+#include "object.h"
 #include "scanner.h"
 
 #ifdef DEBUG_PRINT_CODE
@@ -172,6 +173,12 @@ static void number() {
     emit_constant(NUMBER_VAL(value));
 }
 
+static void string() {
+    // The +1 and -2 trims the leading and trailing quotation marks
+    emit_constant(OBJ_VAL(copy_string(parser.previous.start + 1,
+                                      parser.previous.length - 2)));
+}
+
 static void unary() {
     TokenType operator_type = parser.previous.type;
 
@@ -207,7 +214,7 @@ ParseRule rules[] = {
     [TOKEN_LESS]          = {NULL,     binary, PREC_COMPARISON},
     [TOKEN_LESS_EQUAL]    = {NULL,     binary, PREC_COMPARISON},
     [TOKEN_IDENTIFIER]    = {NULL,     NULL,   PREC_NONE},
-    [TOKEN_STRING]        = {NULL,     NULL,   PREC_NONE},
+    [TOKEN_STRING]        = {string,   NULL,   PREC_NONE},
     [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
     [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
     [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
--- clox/memory.h
+++ clox/memory.h
@@ -2,6 +2,10 @@
 #define clox_memory_h
 
 #include "common.h"
+#include "object.h"
+
+#define ALLOCATE(type, count) \
+    (type*)reallocate(NULL, 0, sizeof(type) * (count))
 
 #define GROW_CAPACITY(capacity) \
     ((capacity) < 8 ? 8 : (capacity) * 2)
@@ -13,6 +17,9 @@
 #define FREE_ARRAY(type, pointer, old_count) \
     reallocate(pointer, sizeof(type) * (old_count), 0)
 
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+
 void* reallocate(void* pointer, size_t old_size, size_t new_size);
+void free_objects();
 
 #endif
